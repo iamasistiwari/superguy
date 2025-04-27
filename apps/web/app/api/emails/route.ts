eea1b768eee5ed17bc7ae557afa9e3e0c5a36db2
iamasistiwari <iamashishtiwari2004@gmail.com>
2025-04-27 21:25:07 +0530

    google-email-oauth

--- apps/web/app/api/emails/route.ts
+++ apps/web/app/api/emails/route.ts
@@ -9,7 +9,7 @@ export async function GET() {
   const session = await getServerSession(authOptions);
   const accessToken = session?.accessToken;
     const checkToken =
-      "ya29.a0AZYkNZhMNCHDdUaoi23CTKXjQq-LlQ5fewup8LRu-6D4iVLL4MWO9H8gjUDkoGBTGq8IEkm5vPhDTOPKm1je42xbTCZBXAf_J0qXFixr5V_W3SGdFCKXuXbr8XruCjQSp10ApWzuY2m2BWqt_auFXUusXd8oW571J8GbV0ctaCgYKAS8SARESFQHGX2MiMW-p_QMjVWJTGnUO5RfEiA0175";
+      "yiur token";
 //   if(!accessToken){
 //     return NextResponse.json("Invalid token")
 //   }
